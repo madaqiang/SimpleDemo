cef123d6c9f5965f173f113c578c5f635deb4240
madaqiang <2621992538@qq.com>
2019-07-25 15:17:55 +0800

    no message

--- src/main/java/com/mdq/simpletest/demo/controller/UserController.java
+++ src/main/java/com/mdq/simpletest/demo/controller/UserController.java
@@ -31,7 +31,6 @@ public class UserController {
         System.out.println();
         System.out.println();
         System.out.println();
-        System.out.println();
         return resultMap;
     }
 
