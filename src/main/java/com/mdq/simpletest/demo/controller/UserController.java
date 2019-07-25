ceaa731e27a8895a7b25441eb3da5185b679e51e
madaqiang <2621992538@qq.com>
2019-07-25 14:26:51 +0800

    4

--- src/main/java/com/mdq/simpletest/demo/controller/UserController.java
+++ src/main/java/com/mdq/simpletest/demo/controller/UserController.java
@@ -31,6 +31,7 @@ public class UserController {
         System.out.println();
         System.out.println();
         System.out.println();
+        System.out.println();
         return resultMap;
     }
 
